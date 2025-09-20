33da299947b5fe2796652d5b2f1e16458908b97b
Karla Patricia Farinas <karlafarinas@gmail.com>
2025-09-20 19:24:21 +0800

    Added admin controller test rspec.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,22 @@
 class Admin::UsersController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_user!
+  before_action :ensure_admin!
 
   def approve
     user = User.find(params[:id])
-    user.update(approved: true)
+    user.update(role: "trader")
     redirect_to admin_users_path, notice: "#{user.username} approved as trader."
   end
 
   def revoke
     user = User.find(params[:id])
-    user.update(approved: false)
-    redirect_to admin_users_path, alert: "#{user.username}'s approval revoked."
+    user.update(role: "user")
+    redirect_to admin_users_path, notice: "#{user.username}'s approval revoked."
+  end
+
+  private
+
+  def ensure_admin!
+    redirect_to root_path, alert: "Not authorized" unless current_user&.admin?
   end
 end
