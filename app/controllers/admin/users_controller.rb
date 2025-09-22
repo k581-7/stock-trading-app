60cd4bf907de0d6faa75bb645566609785d675d8
Karla Patricia Farinas <karlafarinas@gmail.com>
2025-09-22 19:28:09 +0800

    Small changes from app controller.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,21 +2,25 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_admin!
 
+  def index
+    @users = User.all
+  end
+
   def approve
     user = User.find(params[:id])
-    user.update(role: "trader")
+    user.update(approved: true)
     redirect_to admin_users_path, notice: "#{user.username} approved as trader."
   end
 
   def revoke
     user = User.find(params[:id])
-    user.update(role: "user")
-    redirect_to admin_users_path, notice: "#{user.username}'s approval revoked."
+    user.update(approved: false)
+    redirect_to admin_users_path, alert: "#{user.username}'s approval revoked."
   end
 
   private
 
   def ensure_admin!
-    redirect_to root_path, alert: "Not authorized" unless current_user&.admin?
+    redirect_to root_path, alert: "Not authorized" unless current_user&.role == "admin"
   end
 end
