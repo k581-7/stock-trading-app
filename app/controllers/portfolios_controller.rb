8c274a20948b775c3830f481cd98dfa5c0e12827
Karla Patricia Farinas <karlafarinas@gmail.com>
2026-05-05 22:08:23 +0800

    ran rubocop, and updated brakeman gemfile and bundle to clear the CI tests.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_portfolio, only: [:show, :edit, :update, :destroy, :sell]
-  before_action :authorize_portfolio, only: [:show, :edit, :update, :destroy, :sell]
+  before_action :set_portfolio, only: [ :show, :edit, :update, :destroy, :sell ]
+  before_action :authorize_portfolio, only: [ :show, :edit, :update, :destroy, :sell ]
 
   def index
     # User can have multiple portfolios now
